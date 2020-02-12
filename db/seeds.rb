1bc8857f3237a7dd9658e8636a3d382aca11bc1f
quentin de malherbe <quentdemalherbe@gmail.com>
2020-02-12 17:06:12 +0100

    restaurant list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'debut du seed'
+Restaurant.destroy_all
+puts 'destroyed'
+
+puts 'creation'
+Restaurant.create(name: 'coucou', category: 'chinese', address: 'rue')
+Restaurant.create(name: 'hello', category: 'italian', address: 'avenue')
+Restaurant.create(name: 'salut', category: 'chinese', address: 'route')
+Restaurant.create(name: 'bonjour', category: 'japanese', address: 'boulevard')
+Restaurant.create(name: 'hola', category: 'french', address: 'autoroute')
+
+puts 'finished!'
